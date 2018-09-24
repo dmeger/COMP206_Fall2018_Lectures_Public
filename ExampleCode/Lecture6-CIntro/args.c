d591673a0d5f15a081b86aab2c81e84a5b7fece1
David Meger <dmeger@David-Megers-MacBook-Pro.local>
2018-09-24 11:27:27 -0400

    Latest for lec 7

---
+++ ExampleCode/Lecture6-CIntro/args.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main( int argc, char *argv[] ){
+
+	printf( "I have %d args.\n", argc );
+	printf( "First was %s.\n", argv[0] );
+	printf( "Second was %s.\n", argv[1] );
+
+}
---
+++ ExampleCode/Lecture7-TextData/max_int_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+	int val=0;
+
+	while( 1 )
+		printf( "Val holds: %d.\n", val++ );
+
+}
